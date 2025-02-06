e1636c03418172c231dd4d1174f7aab5e87f9f0b
Baptiste <Bcoquet03@gmail.com>
2025-02-07 00:39:16 +0100

    Model Finit(peut-etre)

--- app/Model/ProductModel.rb
+++ app/Model/ProductModel.rb
@@ -3,11 +3,12 @@ require_relative "../Core/DB/DB.rb"
 module App
     module Model
         class ProductModel < Core::DB::DB
-            def initialize(id,name,price)
+            def initialize(id,name,price,description)
+                super()
                 @product_id = id
                 @product_name = name
                 @product_price = price
-                super()
+                @product_description = description  
             end
 
             def get_product(product_id)
@@ -18,12 +19,20 @@ module App
                 query("SELECT * FROM product")
             end
 
-            def set_product_name(product_name,product_id)
+            def set_product_name(product_id,product_name)
                 query("UPDATE product SET product_name = ? WHERE id = ? ",product_name,product_id)
             end
 
-            def set_product_price(product_price,product_id)
+            def set_product_price(product_id,product_price)
                 query("UPDATE product SET product_price = ? WHERE id = ?",product_price,product_id)
+            end
+
+            def set_product_description(product_id,product_description)
+                query("UPDATE product SET product_description = ? WHERE id = ?",product_description,product_id)
+            end
+
+            def add_product(product_name,product_price,product_description)
+                query("INSERT INTO product(product_name,product_price,product_description) VALUES (?,?,?)",product_name,product_price,product_description)
             end
         end
     end
